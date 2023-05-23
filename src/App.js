6e17d226b271074fbe619112ee951c8a773149b4
Kohta9521 <kohta0421kochi@gmail.com>
2023-05-23 21:24:19 +0900

    #2 complete firebase and firestore database connect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import db from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
-  return <div className="App"></div>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    // データベースからデータを取得する
+    const postData = collection(db, "portfolio");
+    console.log(postData);
+    getDocs(postData).then((snapShot) => {
+      console.log(snapShot.docs.map((doc) => ({ ...doc.data() })));
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1></h1>
+    </div>
+  );
 }
 
 export default App;
